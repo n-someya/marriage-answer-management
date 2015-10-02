06cfcb730a6fadc7ca5ac0537c45e5a22d6e8107
n-someya <n.someya.ynu@gmail.com>
2015-10-02 11:20:34 +0200

    FIX: describe ranking

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
     def index
 
-        @users=User.find_by_sql("select user_id, user_name, count(*) as cnt from answers left join corrects using (stage) left join users on answers.user_id = users.id  where answer = correct group by user_id, user_name order by cnt desc;")
+        @users=User.find_by_sql("select user_id, user_name, count(*) as cnt , rank() over ( order by count(*) desc ) from answers left join corrects using (stage) left join users on answers.user_id = users.id  where answer = correct group by user_id, user_name order by cnt desc;")
         ans=Answer.find_by_sql("select stage, answer ,count(*) as cnt from answers group by stage, answer order by stage ,answer;")
         max=Answer.maximum('stage')
         if max == nil
